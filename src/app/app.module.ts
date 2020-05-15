eddb914f780a4023f85528a4be2ee9052ecd7fe1
adrianb-badulescu <adrianb2104@gmail.com>
2020-05-15 11:54:35 +0300

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,3 @@
-
-
-
 import { NgModule } from '@angular/core';
 import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -10,41 +7,26 @@ import { GestureConfig } from '@angular/material';
 import { NgxSmartModalModule } from 'ngx-smart-modal';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatSliderModule } from '@angular/material/slider';
 
-// import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-// import { FakeDbService } from './fake-data/_fake_data.service';
-
-
-// import {environment} from 'src/environments/environment';
-// import { AngularFirestoreModule } from 'angularfire2/firestore';
-// import { AngularFireModule } from 'angularfire2';
-// import { AngularFireDatabaseModule } from 'angularfire2/database';
-// import { AngularFireAuthModule } from 'angularfire2/auth';
-
-//import {CalendarModule} from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
 import { TableModule } from 'primeng/table';
 import { TreeTableModule } from 'primeng/treetable';
 import { TreeModule } from 'primeng/tree';
-// import { TreeNode } from 'primeng/api';
-//import {DataTableModule} from 'primeng/datatable';
+
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { CheckboxModule } from 'primeng/checkbox';
 import { DialogModule } from 'primeng/dialog';
 import { CodeHighlighterModule } from 'primeng/codehighlighter';
 import { TabViewModule } from 'primeng/tabview';
-/*import {TabPanel} from 'primeng/tabpanel';*/
-/*import {Column} from 'primeng/column';*/
+
 import { ContextMenuModule } from 'primeng/contextmenu'
-/*import {MenuItem, Header, Footer} from 'primeng/components/common/api';*/
-// import {EditorModule} from 'primeng/editor';
+
 import { FileUploadModule } from 'primeng/fileupload';
 
 import { ErrorInterceptor } from './core/helpers/error.interceptor';
 import { JwtInterceptor } from './core/helpers/jwt.interceptor';
-// import { FakeBackendProvider } from './core/helpers/fake-backend';
+
 
 import { LayoutsModule } from './layouts/layouts.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -77,16 +59,9 @@ import { environment } from '../environments/environment';
     
 
     NgxSmartModalModule.forRoot(),
-    // InMemoryWebApiModule.forRoot(FakeDbService),
 
     PipesModule,
 
-    // AngularFireModule.initializeApp(environment.firebase, 'angular-auth-firebase'),
-    // AngularFirestoreModule,
-    // AngularFireDatabaseModule,
-    // AngularFireAuthModule,  
-
-    //CalendarModule, DataTableModule, EditorModule
     DropdownModule, TableModule, TreeTableModule, TreeModule, TabViewModule, CodeHighlighterModule, DialogModule, ButtonModule, InputTextModule, CheckboxModule,
     ContextMenuModule, FileUploadModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
